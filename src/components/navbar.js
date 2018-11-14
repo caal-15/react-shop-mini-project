0c647ac5f390d3089c58470bb319a79013fb3ae6
Carlos Gonzalez <caal.0522@gmail.com>
2018-11-14 09:17:34 -0500

    Adds active function to navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,12 +5,18 @@ import { Link } from 'react-router-dom'
 import navOptions from '../config/nav'
 
 const Navbar = ({ location }) => {
+  const primaryPath = location.pathname.split('/')[1]
   const children = (
     <nav className='topnav'>
       {navOptions.map((option, idx) => {
+        const btnPrimaryPath = option.to.split('/')[1]
+        const btnClass = primaryPath === btnPrimaryPath
+          ? 'topnav__link__btn topnav__link__btn--active'
+          : 'topnav__link__btn'
+
         return (
           <Link key={idx} className='topnav__link' to={option.to}>
-            <Button className='topnav__link__btn' flat>
+            <Button className={btnClass} flat>
               {option.text}
             </Button>
           </Link>
