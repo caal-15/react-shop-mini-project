88b86766cf6d3b425cddee7fe1d7dd1e9201d375
Carlos Gonzalez <caal.0522@gmail.com>
2018-11-13 19:40:48 -0500

    Adds React Router.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
 import './scss/index.sass'
+
+import Home from './components/home'
 
 const App = () => {
   return (
-    <h1>Hello</h1>
+    <BrowserRouter>
+      <div>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/products' component={Home} />
+        <Route exact path='/products/:filter' component={Home} />
+        <Route exact path='/products/clients' component={Home} />
+        <Route exact path='/contact' component={Home} />
+      </div>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Home = () => {
+  return (
+    <header>
+      <h1>Home</h1>
+    </header>
+  )
+}
+
+export default Home
