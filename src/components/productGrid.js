201342c5b3d20d25150d0969219c88305f74a59d
Carlos Gonzalez <caal.0522@gmail.com>
2018-11-14 18:58:00 -0500

    Adds Grid/List Functionality.

---
+++ src/components/productGrid.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { Grid, Cell, Card, CardText, CardTitle } from 'react-md'
+
+const ProductGrid = ({ products }) => {
+  return (
+    <Grid>
+      {products.map(product => {
+        const subtitle = `${product.categories.join(', ')} - ${product.brand}`
+        return (
+          <Cell key={product.id} size={6} align={'stretch'}>
+            <Card className='products__grid__card'>
+              <CardTitle title={product.name} subtitle={subtitle} />
+              <div className='products__grid__card__info'>
+                <div className='products__grid__card__img__wrapper'>
+                  <img
+                    className='products__grid__card__img'
+                    src='https://picsum.photos/640/480/?random'
+                    alt={`${product.name} image`}
+                  />
+                </div>
+                <div className='products__grid__card__text__wrapper'>
+                  <CardText className='products__list__card__text'>
+                    <p>{product.description}</p>
+                    <p><strong>Stock:</strong> {product.stock}</p>
+                    <p><strong>Price:</strong> ${product.price}</p>
+                  </CardText>
+                </div>
+              </div>
+            </Card>
+          </Cell>
+        )
+      })}
+    </Grid>
+  )
+}
+
+export default ProductGrid
--- src/components/products.js
+++ src/components/products.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
-import { CircularProgress, Snackbar } from 'react-md'
+import { CircularProgress, Snackbar, Button } from 'react-md'
 
 import ProductList from './productList'
+import ProductGrid from './productGrid'
 
 class Products extends Component {
   constructor(props) {
     super(props)
+    this.state = { list: true }
+    this.showList = this.showList.bind(this)
+    this.showGrid = this.showGrid.bind(this)
   }
 
   componentDidMount() {
@@ -13,17 +17,33 @@ class Products extends Component {
     fetchProducts()
   }
 
+  showList() {
+    this.setState({ list: true })
+  }
+
+  showGrid() {
+    this.setState({ list: false })
+  }
+
   render() {
     const { fetching, products, error, dismissError } = this.props
+    const { list } = this.state
     const toasts = error ? [{ text: error }] : []
-    console.log(products)
     return (
       <section className='products'>
+        <div className='products__toolbar'>
+          <div className ='products__toolbar__mode'>
+            <Button onClick={this.showList} icon>list</Button>
+            <Button onClick={this.showGrid} icon>view_module</Button>
+          </div>
+        </div>
         {fetching
           ? <div className='wait-container'>
             <CircularProgress id='products-loading' />
           </div>
-          : <ProductList products={products} />
+          : list
+            ? <ProductList products={products} />
+            : <ProductGrid products={products} />
         }
         <Snackbar
           id='products-error'
