d891640585ca89d06736000dd1ad15727319699b
Carlos Gonzalez <caal.0522@gmail.com>
2018-11-16 07:41:30 -0500

    Renames some definitions to avoid (posible) collisions in redux.

--- src/definitions/messages.js
+++ src/definitions/messages.js
@@ -1,6 +1,6 @@
 const definitions = {
-  SET_FECTHING: 'SET_FETCHING',
-  SET_ERROR: 'SET_ERROR'
+  SET_FECTHING: 'SET_FETCHING_MESSAGES',
+  SET_ERROR: 'SET_ERROR_MESSAGES'
 }
 
 export default definitions
