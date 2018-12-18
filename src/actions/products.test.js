0121909df3904617ea76e29f3c396418c5764f50
Carlos Gonzalez <caal.0522@gmail.com>
2018-12-18 17:14:01 -0500

    Adds first Action creator test.

---
+++ src/actions/products.test.js
@@ -0,0 +1,46 @@
+import axios from 'axios'
+import MockAdapter from 'axios-mock-adapter'
+import configureMockStore from 'redux-mock-store'
+
+import serverConf from '../config/server'
+import { fetchProducts } from '../actions/products'
+
+const flushAllPromises = () => new Promise(resolve => setImmediate(resolve))
+
+describe('When Fetching Products', () => {
+
+  let store
+  let httpMock
+
+  beforeEach(() => {
+    httpMock = new MockAdapter(axios)
+    const mockStore = configureMockStore()
+    store = mockStore({})
+  })
+
+  it('works', async () => {
+    const expected = [
+      {
+        id: 'MaId',
+        name: 'DaProduct',
+        brand: 'DaSupaBrand',
+        categories: ['DaCat', 'DaOtherCat'],
+        description: 'A really good one',
+        photo: 'DaPhoto',
+        price: '10900',
+        stock: 100
+      }
+    ]
+    httpMock.onGet(serverConf.url + '/products').reply(200, expected)
+
+    fetchProducts(store.dispatch)()
+
+    await flushAllPromises()
+
+    expect(store.getActions()).toEqual([
+      { type: 'SET_FETCHING', fetching: true },
+      { type: 'FETCH_PRODUCTS', products: expected },
+      { type: 'SET_FETCHING', fetching: false }
+    ])
+  })
+})
